6d22ae98efd8516066128fbf014f3ebf4bd39b5a
Lucas Guerra <lucasguerra@owl-dev.com.ar>
2017-08-17 01:22:11 -0300

    Poniendome al dia

--- src/poo/UsoEmpleado.java
+++ src/poo/UsoEmpleado.java
@@ -13,11 +13,24 @@ public class UsoEmpleado {
        muestraEmpleado(empleado1);
        muestraEmpleado(empleado2);
         */
+       Jefatura jefe_rrhh = new Jefatura("Alberto Elcho", 75000,2010,1,12);
+       jefe_rrhh.setIncentivo(5000);
 
-        Empleado[] misEmpleados = new Empleado[3];
+        Empleado[] misEmpleados = new Empleado[5];
         misEmpleados[0] = new Empleado("Paco Gomez",85000,1990,12,17);
         misEmpleados[1] = new Empleado("Ana Lopez",95000,1995,6,2);
         misEmpleados[2] = new Empleado("Maria Martin",105000,2000,8,10);
+        misEmpleados[3] = jefe_rrhh;
+        misEmpleados[4] = new Jefatura("Maria", 95000, 1999 ,12 , 1 );
+
+        //Casting
+        // misEmpleados[4] no permite acceder a setIncentivo
+        Jefatura jefaFinanzas = (Jefatura) misEmpleados[4];
+        jefaFinanzas.setIncentivo(5000); // En cambio, al hacer un casting, si podemos acceder a los metodos de la clase Jefatura
+
+
+
+        // System.out.println(misEmpleados[3].getAltaContrato());  Se puede acceder a los metodos desde la instancia
 
         /*
         for (int i = 0; i <3 ; i++){
@@ -52,74 +65,75 @@ public class UsoEmpleado {
 
 
 // CLASE EMPLEADO
-class Empleado{
+    class Empleado{
 
-    // Constructor de la clase
-    public Empleado(String nom, double sue, int y,  int m, int d ){
-        nombre = nom;
-        sueldo = sue;
-        GregorianCalendar calendario = new GregorianCalendar(y,m-1,d);
-        altaContrato = calendario.getTime();
+                // Constructor de la clase
+                public Empleado(String nom, double sue, int y,  int m, int d ){
+                    nombre = nom;
+                    sueldo = sue;
+                    GregorianCalendar calendario = new GregorianCalendar(y,m-1,d);
+                    altaContrato = calendario.getTime();
+                }
+
+                // GETTERS
+
+                public String getNombre(){
+                    return nombre;
+                }
+
+                public double getSueldo(){
+                    return sueldo;
+                }
+
+                public Date getAltaContrato(){
+                    return altaContrato;
+                }
+
+                //SETTERS
+
+                public void setNombre(String nombre) {
+                    this.nombre = nombre;
+                }
+
+                public void setSueldo(double sueldo) {
+                    this.sueldo = sueldo;
+                }
+
+                public void setAltaContrato(Date altaContrato) {
+                    this.altaContrato = altaContrato;
+                }
+
+                // Aumento de sueldo
+                public void subeSueldo(double porcentaje){
+                    double aumento = sueldo*porcentaje/100;
+                    sueldo+= aumento;
+                }
+
+
+                // Campos de clase
+                private String nombre;
+                private double sueldo;
+                private Date altaContrato;
+
     }
 
-    // GETTERS
+    // SubClase Jefatura que se extiende de la superclase Empleado
+    class Jefatura extends Empleado{
 
-    public String getNombre(){
-        return nombre;
+                public Jefatura(String nom, double sue, int y , int m , int d){
+                    super(nom,sue,y,m,d);
+                }
+
+                public void setIncentivo(double b){
+                    incentivo = b;
+                }
+
+                public double getSueldo(){
+                    double sueldoJefe = super.getSueldo();
+                    return sueldoJefe + incentivo;
+                }
+
+
+                // Campos de clase siempre al final o al inicio de una clase
+                private double incentivo;
     }
-
-    public double getSueldo(){
-        return sueldo;
-    }
-
-    public Date getAltaContrato(){
-        return altaContrato;
-    }
-
-    //SETTERS
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setSueldo(double sueldo) {
-        this.sueldo = sueldo;
-    }
-
-    public void setAltaContrato(Date altaContrato) {
-        this.altaContrato = altaContrato;
-    }
-
-    // Aumento de sueldo
-    public void subeSueldo(double porcentaje){
-        double aumento = sueldo*porcentaje/100;
-        sueldo+= aumento;
-    }
-
-
-    // Campos de clase
-    private String nombre;
-    private double sueldo;
-    private Date altaContrato;
-
-}
-
-class Jefatura extends Empleado{
-    public Jefatura(String nom, double sue, int y , int m , int d){
-        super(nom,sue,y,m,d);
-    }
-
-    public void setIncentivo(double b){
-        incentivo = b;
-    }
-
-    public double getSueldo(){
-        double sueldoJefe = super.getSueldo();
-        return sueldoJefe + incentivo;
-    }
-
-
-    // Campos de clase siempre al final o al inicio de una clase
-    private double incentivo;
-
-}
