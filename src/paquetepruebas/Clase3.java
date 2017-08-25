e545b3e3ef87730a6ba357cccd229ca87aae0b9e
Lucas Guerra <guerra986@gmail.com>
2017-08-25 00:01:48 -0300

    listo para pushear

--- src/paquetepruebas/Clase3.java
+++ src/paquetepruebas/Clase3.java
@@ -2,7 +2,7 @@ package paquetepruebas;
 
 import poo.Clase1;
 
-public class Clase3 {
+public class Clase3 extends Clase1{
     Clase1 miobj = new Clase1();
 
 }
--- src/poo/Clase2.java
+++ src/poo/Clase2.java
@@ -1,9 +1,19 @@
 package poo;
 
+
+import paquetepruebas.Clase3;
+
 public class Clase2 {
     public static void main(String[] args){
 
+        // Creando objetos correspondientes a clases que se encuentran en otros paquetes
         Clase1 miobj = new Clase1();
+        Clase3 miobj2= new Clase3();
+
+        // Modificando el objeto creado y utilizando un  metodo propio de su clase
+        miobj2.mivar = 7;
+        System.out.println(miobj2.mimetodo());
+
 
     }
 }
